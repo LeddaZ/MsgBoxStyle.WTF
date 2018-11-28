ada1a982934211840780f92f325c27564c734544
Leonardo Ledda <35930217+LeddaZ@users.noreply.github.com>
2018-11-28 20:15:06 +0100

    Add files via upload

---
+++ MsgBoxStyle.WTF/Module1.vb
@@ -0,0 +1,29 @@
+﻿Module Module1
+
+    Sub Main()
+        Console.Title = "MsgBoxStyle.WTF"
+        Console.ForegroundColor = ConsoleColor.Yellow
+        Console.Clear()
+        Console.WriteLine("MsgBoxStyle.WTF")
+        Console.WriteLine()
+        Console.WriteLine("MsgBox utilizzate nello sticker pack MsgBoxStyle.WTF su Telegram (https://t.me/addstickers/MsgBoxStyle.WTF)")
+        MsgBox("Vuoi ristabilire il comunismo?", Microsoft.VisualBasic.MsgBoxStyle.YesNo, "MsgBoxStyle.WTF")
+        MsgBox("Sei autistico", Microsoft.VisualBasic.MsgBoxStyle.OkOnly, "MsgBoxStyle.WTF")
+        MsgBox("Gay chi legge", Microsoft.VisualBasic.MsgBoxStyle.OkOnly, "MsgBoxStyle.WTF")
+        MsgBox("È stato rilevato un eccesso di cromosomi", Microsoft.VisualBasic.MsgBoxStyle.Critical, "MsgBoxStyle.WTF")
+        MsgBox("Porco Dio", Microsoft.VisualBasic.MsgBoxStyle.OkOnly, "MsgBoxStyle.WTF")
+        MsgBox("Smettila di segarti sugli hentai", Microsoft.VisualBasic.MsgBoxStyle.OkOnly, "MsgBoxStyle.WTF")
+        MsgBox("Fai schifo", Microsoft.VisualBasic.MsgBoxStyle.OkOnly, "MsgBoxStyle.WTF")
+        MsgBox("Hai vinto un iPhone XS Max da 512GB", Microsoft.VisualBasic.MsgBoxStyle.OkOnly, "MsgBoxStyle.WTF")
+        MsgBox("Madonna puttana", Microsoft.VisualBasic.MsgBoxStyle.OkOnly, "MsgBoxStyle.WTF")
+        MsgBox("Ti meriti una ginocchiata sui coglioni", Microsoft.VisualBasic.MsgBoxStyle.OkOnly, "MsgBoxStyle.WTF")
+        MsgBox("Qualcuno ha una corda?", Microsoft.VisualBasic.MsgBoxStyle.OkOnly, "MsgBoxStyle.WTF")
+        MsgBox("Impossibile continuare - È stata rilevata la presenza di un coglione", Microsoft.VisualBasic.MsgBoxStyle.Critical, "MsgBoxStyle.WTF")
+        MsgBox("La persona sopra è ritardata", Microsoft.VisualBasic.MsgBoxStyle.OkOnly, "MsgBoxStyle.WTF")
+        MsgBox("Quanti cromosomi hai? Sono più di 300 vero?", Microsoft.VisualBasic.MsgBoxStyle.YesNo, "MsgBoxStyle.WTF")
+        MsgBox("Se non la smettete di rompere il cazzo vi prendo a cinghiate sui denti Dio cancro", Microsoft.VisualBasic.MsgBoxStyle.OkOnly, "MsgBoxStyle.WTF")
+        Console.WriteLine()
+        Console.WriteLine("Sticker creati da @LeddaZ")
+        Console.ReadKey()
+    End Sub
+End Module
